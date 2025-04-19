2124d1ef7a0a5a3530de7f6927e98d68a297b0f8
Shradha Bidwai <41946486+ShradhaBidwai@users.noreply.github.com>
2025-04-19 15:32:35 +0530

    remove cooment in view.py

--- tutor/models.py
+++ tutor/models.py
@@ -11,8 +11,3 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-
-# {
-#     "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc0NTA0NTE3OCwiaWF0IjoxNzQ0OTU4Nzc4LCJqdGkiOiIyNTdkOGZlMTU5NGI0YWViYThmN2MwN2JlNGQxOTkzZSIsInVzZXJfaWQiOjJ9.Q6_hbw_-0VB1IQM97rI4ZDttJM3ufmeaJqSdMGBWR6w",
-#     "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ0OTU5MDc4LCJpYXQiOjE3NDQ5NTg3NzgsImp0aSI6ImRkOTJmZDljZGZkMTRmN2JiNGYyYTVhNTFiZDc2MWZhIiwidXNlcl9pZCI6Mn0.BksApZSYg7_1d_BqcW_mVaYoXN2VGwQi4UFd51WHS3A"
-# }
