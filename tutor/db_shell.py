bbc752c2a7760d43f2a71222611ba93ee284ccd2
Shradha Bidwai <shradhabidwai95@gmail.com>
2025-04-19 17:55:50 +0530

    test

--- tutor/db_shell.py
+++ tutor/db_shell.py
@@ -1,10 +1,10 @@
 
-from datetime import date
-p1 = book.object.create(title = "ecv",author ="hdjhsgj",publish_date = date(10,10,2025))
-p1.save() 
+# from datetime import date
+# p1 = book.object.create(title = "ecv",author ="hdjhsgj",publish_date = date(10,10,2025))
+# p1.save() 
 
-#######
+# #######
 
-p2 = book.object.create(title ="ecv2",author ="abcded",publish_date = date(20,10,2025))
-p2.save() 
+# p2 = book.object.create(title ="ecv2",author ="abcded",publish_date = date(20,10,2025))
+# p2.save() 
 
