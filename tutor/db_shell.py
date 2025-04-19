1436cc001184d2062df928b13def8164d63379c4
Shradha Bidwai <shradhabidwai95@gmail.com>
2025-04-19 16:33:27 +0530

    code added in db_shell.py file

--- tutor/db_shell.py
+++ tutor/db_shell.py
@@ -4,3 +4,7 @@ p1 = book.object.create(title = "ecv",author ="hdjhsgj",publish_date = date(10,1
 p1.save() 
 
 #######
+
+p2 = book.object.create(title ="ecv2",author ="abcded",publish_date = date(20,10,2025))
+p2.save() 
+
